94106002264e373465ace61a21817cf7fb9eb513
Patrik Majer <patrik.majer.pisek@gmail.com>
2019-10-06 12:46:17 +0200

    add functions and defer

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"fmt"
 	"rsc.io/quote"
+	"time"
 )
 
 //go mod https://github.com/golang/go/wiki/Modules#example
 
+func printCustomDate() string {
+	dt := time.Now()
+	dtf := dt.Format("01-02-2006 15:04:05")
+	//dtf := dt.String()
+
+	fmt.Println("Dnes je: ", dtf)
+
+	return dtf
+}
+
+func on_finish() {
+	fmt.Println("Finished")
+}
+
 func main() {
+	defer on_finish()
+
 	fmt.Println(quote.Hello())
 	fmt.Println("ahoj svete")
+
+	printCustomDate()
 
 	//https://github.com/davecgh/go-spew
 }
